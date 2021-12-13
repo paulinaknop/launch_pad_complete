723826575e91974827858c257fc2f3a20b5eeaed
paulinaeknop@gmail.com <5!Coo8kQi>
2021-12-13 19:35:19 +0530

    Kotlin version updated.

--- lib/main.dart
+++ lib/main.dart
@@ -93,10 +93,11 @@ class _PadState extends State<Pad> {
     double height = MediaQuery.of(context).size.height;
     return GestureDetector(
       onTap: () async {
+        player.play(widget.note);
         setState(() {
           colorCenter = Colors.white;
           colorOutline = Colors.white;
-          player.play(widget.note);
+
         });
         await Future.delayed(const Duration(milliseconds: 500));
         setState(() {
